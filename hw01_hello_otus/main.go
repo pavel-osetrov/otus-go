76d1b00fdd0110c5857554df092be382e3d41848
p.osetrov <p.osetrov@b2b-center.ru>
2024-04-14 21:41:52 +0300

    HW2 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,12 +1,66 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"golang.org/x/example/stringutil"
+	"strconv"
+	"strings"
 )
 
+var ErrInvalidString = errors.New("invalid string")
+
+func Unpack(str string) (string, error) {
+
+	var builder strings.Builder
+
+	var previousValue rune
+
+	for key, value := range str {
+
+		IsNumberValue := IsNumber(value)
+		IsNumberPreviousValue := IsNumber(previousValue)
+		IsZero := value == '0'
+
+		if key == 0 && IsNumberValue {
+			return "", ErrInvalidString
+		}
+
+		if IsNumberPreviousValue && IsNumberValue {
+			return "", ErrInvalidString
+		}
+
+		if IsNumberValue && IsZero {
+			str := builder.String()
+			updatedStr := str[:len(str)-1]
+			builder.Reset()
+			builder.WriteString(updatedStr)
+		}
+
+		if IsNumberValue && !IsZero {
+			count, err := strconv.Atoi(string(value))
+			if err != nil {
+				return "", err
+			}
+
+			repeated := strings.Repeat(string(previousValue), count-1)
+			builder.WriteString(repeated)
+		}
+
+		if !IsNumberValue {
+			builder.WriteString(string(value))
+		}
+		previousValue = value
+
+	}
+
+	return builder.String(), nil
+}
+
+func IsNumber(value rune) bool {
+	return '0' <= value && value <= '9'
+}
+
 func main() {
-	reverse := stringutil.Reverse("Hello, OTUS!")
-	fmt.Println(reverse)
+
+	fmt.Println(Unpack("aaa10b"))
 }
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+
+	var builder strings.Builder
+
+	var previousValue rune
+
+	for key, value := range str {
+
+		IsNumberValue := IsNumber(value)
+		IsNumberPreviousValue := IsNumber(previousValue)
+		IsZero := value == '0'
+
+		if key == 0 && IsNumberValue {
+			return "", ErrInvalidString
+		}
+
+		if IsNumberPreviousValue && IsNumberValue {
+			return "", ErrInvalidString
+		}
+
+		if IsNumberValue && IsZero {
+			str := builder.String()
+			updatedStr := str[:len(str)-1]
+			builder.Reset()
+			builder.WriteString(updatedStr)
+		}
+
+		if IsNumberValue && !IsZero {
+			count, err := strconv.Atoi(string(value))
+			if err != nil {
+				return "", err
+			}
+
+			repeated := strings.Repeat(string(previousValue), count-1)
+			builder.WriteString(repeated)
+		}
+
+		if !IsNumberValue {
+			builder.WriteString(string(value))
+		}
+		previousValue = value
+
+	}
+
+	return builder.String(), nil
+}
+
+func IsNumber(value rune) bool {
+	return '0' <= value && value <= '9'
 }
